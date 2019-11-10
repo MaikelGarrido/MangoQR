70620eaa190cf1e0199c03ebf3cef1afa195f7d1
MaikelG <maikel.garrido0@gmail.com>
2019-11-10 07:10:43 -0430

    Commit prueba

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -13,23 +13,18 @@ export class AuthService {
   constructor(private AFauth: AngularFireAuth, private router: Router ) { }
 
   login(cedula: string, password: string) {
-
 // tslint:disable-next-line: no-shadowed-variable
     return new Promise((resolve, rejected) => {
-
       this.AFauth.auth.signInWithEmailAndPassword(cedula, password).then(user => {
         resolve(user);
       }).catch(err => rejected(err));
     });
-
   }
 
   logout() {
-
     this.AFauth.auth.signOut().then (() => {
       this.router.navigate(['./login']);
     });
   }
-
 
 }

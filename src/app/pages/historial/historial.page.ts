9cb17da3209253b661391017a77db05285fd6116
Maikel Garrido <maikel.garrido0@gmail.com>
2020-02-04 14:01:52 -0400

    Filtro de fechas con reporte PDF

--- src/app/pages/historial/historial.page.ts
+++ src/app/pages/historial/historial.page.ts
@@ -5,6 +5,7 @@ import * as pdfmake from 'pdfmake/build/pdfmake';
 import * as pdfFonts from 'pdfmake/build/vfs_fonts';
 import { File } from '@ionic-native/file/ngx';
 import { NavController, ToastController } from '@ionic/angular';
+import { map } from 'rxjs/operators';
 
 
 export interface QR {
@@ -25,6 +26,7 @@ export class HistorialPage implements OnInit {
 
   private itemsCollection: AngularFirestoreCollection<QR>;
   private QR: Observable<QR[]>;
+  private data = [];
 
   startDate = new Date().toISOString().replace(/T.*/, '').split('-').reverse().join('/');
   endDate = new Date().toISOString().replace(/T.*/, '').split('-').reverse().join('/');
@@ -34,12 +36,9 @@ export class HistorialPage implements OnInit {
               public file: File,
               public toastCtrl: ToastController) {
     // this.itemsCollection = db.collection<QR>('QR');
+
     // this.itemsCollection = db.collection<QR>('QR', ref => ref.orderBy('createdAt', 'desc'));
     // this.QR = this.itemsCollection.valueChanges();
-
-    // this.db.collection('QR').doc('EDbal1KTfmlZg1WTlu0r').get().forEach(doc => {
-    //   console.log(doc.data().createdAt);
-    // });
   }
 
   loadResults() {
@@ -55,18 +54,30 @@ export class HistorialPage implements OnInit {
         return;
     }
 
-    this.itemsCollection = this.db.collection('QR', ref => ref.where('createdAt', '>=', fecha1) && ref.where('endDate', '<=', fecha2));
+    this.itemsCollection = this.db.collection('QR', ref =>
+    ref.where('createdAt', '>=', fecha1)
+    && ref.where('endDate', '<=', fecha2));
+
     this.QR = this.itemsCollection.valueChanges();
+    this.data = [];
+
+    const userDoc = this.itemsCollection;
+    userDoc.get().subscribe((querySnapshot) => {
+      querySnapshot.forEach((doc => {
+        // console.log(doc.id, '=>', doc.data());
+        this.data.push(doc.data());
+      }));
+    });
 
     // this.db.collection('QR', ref => ref.where('createdAt', '>=', fecha1) && ref.where('endDate', '<=', fecha2)).get().subscribe(ref => {
     //   const jsonvalue: any[] = [];
     //   ref.forEach(docs => {
-    //     jsonvalue.push(this.getAllQR);
+    //     jsonvalue.push(docs.id);
     //   });
     //   console.log(jsonvalue);
     //   return;
-    //   }).catch( error => {
-    //       res.status(500).send(error);
+    //   // }).catch( error => {
+    //   //     res.status(500).send(error);
     // });
   }
 
@@ -82,6 +93,7 @@ getAllQR() {
   }
 
   // Mostrar QR's registrados al abrir la página
+
 ngOnInit() {
     // tslint:disable-next-line: no-shadowed-variable
     // this.getAllQR().subscribe(QR => {
@@ -90,51 +102,64 @@ ngOnInit() {
   }
 
   // Método para tomar el día
-  dateToday() {
-    const date = new Date().toISOString().slice(0, 10);
-    this.startDate = date.split('-')[2] + '-' + date.split('-')[1] + '-' + date.split('-')[0];
-  }
+  // dateToday() {
+  //   const date = new Date().toISOString().slice(0, 10);
+  //   this.startDate = date.split('-')[2] + '-' + date.split('-')[1] + '-' + date.split('-')[0];
+  // }
 
   // Generando tabla y pdf
-  cantWidths(columns) {
-    const widths = [];
-    columns.forEach(() => {
-      widths.push('auto');
-    });
-    return widths;
-  }
+  // cantWidths(columns) {
+  //   const widths = [];
+  //   columns.forEach(() => {
+  //     widths.push('auto');
+  //   });
+  //   return widths;
+  // }
 
-  buildTableBody(data, columns) {
-      const body = [];
-      body.push(columns);
-      // tslint:disable-next-line:only-arrow-functions
-      data.forEach(function(row) {
-          const dataRow = [];
-          // tslint:disable-next-line:only-arrow-functions
-          columns.forEach(function(column) {
-              dataRow.push(row[column].toString());
-          });
-          body.push(dataRow);
-      });
-      return body;
-  }
+  // buildTableBody(data, columns) {
+  //     const body = [];
+  //     body.push(columns);
+  //     // tslint:disable-next-line:only-arrow-functions
+  //     data.forEach(function(row) {
+  //         const dataRow = [];
+  //         // tslint:disable-next-line:only-arrow-functions
+  //         columns.forEach(function(column) {
+  //             dataRow.push(row[column].toString());
+  //         });
+  //         body.push(dataRow);
+  //     });
+  //     return body;
+  // }
 
-  table(data, columns) {
-      return {
-          table: {
-            widths: this.cantWidths(columns),
-              headerRows: 1,
-              body: this.buildTableBody(data, columns)
-          }
-      };
-  }
+  // table(data, columns) {
+  //     return {
+  //         table: {
+  //           widths: this.cantWidths(columns),
+  //             headerRows: 1,
+  //             body: this.buildTableBody(data, columns)
+  //         }
+  //     };
+  // }
 
   generarPDF() {
     pdfmake.vfs = pdfFonts.pdfMake.vfs;
+    const data = [
+      ['Cédula', 'Serial', 'Fecha escaneado']
+    ];
+
+    this.data.forEach(user =>{
+      data.push([user.cedula, user.serial, user.createdAt]);
+      console.log('position', user);
+    });
+
+
     const docDefinition = {
+      footer: (currentPage, pageCount) =>
+      ({ text: currentPage.toString() + ' de ' + pageCount, alignment: 'center'}),
+
       content: [
         {
-        text: this.dateToday(),
+        text: '',
         width: '*',
         alignment: 'right',
         style: 'small'
@@ -147,36 +172,22 @@ ngOnInit() {
       },
       {
         width: '*',
-        alignment: 'left',
+        alignment: 'center',
         style: 'subheader',
-        text: 'Registro de todas las familias escaneadas desde  ' +
-        new Date(this.startDate).toISOString().replace(/T.*/, '').split('-').reverse().join('/')
-        + '  hasta  ' + new Date(this.endDate).toISOString().replace(/T.*/, '').split('-').reverse().join('/')
+        text: 'Registro de todas las familias escaneadas'
       },
       {
-        style: 'tableExample',
-        table: {
-          body: [
-            ['ID', 'Cédula', 'Serial', 'createdAt']
-          ]
-        }
+        columns: [
+          { width: '*', text: ''},
+          {
+            width: 'auto',
+            table: {
+              body: data
+            }
+          },
+          { width: '*', text: ''}
+        ]
       }
-        // this.table(this.jsonvalue, ['id', 'cedula', 'serial', 'createdAt', 'endDate'])
-          // {
-          //   columns: [
-          //     [{
-          //       text: 'Firstname : '
-          //     },
-          //     {
-          //       text: 'Lastname : '
-          //     },
-          //     {
-          //       text: 'Username : '
-          //     },
-          //     {
-          //       text: 'Email : '
-          //     }]
-          //   ]}
       ],
       styles: {
       header: {
@@ -190,7 +201,8 @@ ngOnInit() {
         margin: [0, 10, 0, 5]
       },
       tableExample: {
-        margin: [0, 5, 0, 15]
+        margin: [0, 5, 0, 15],
+
       },
       tableHeader: {
         bold: true,
